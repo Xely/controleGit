f3f435cda885eb9e1d4591de48aceadcc8eee50f
Xely <julien_guya@msn.com>
2017-01-10 16:04:19 +0100

    user class

---
+++ src/core/entities/User.java
@@ -0,0 +1,38 @@
+public class User {
+    public String nom;
+    public String prenom;
+    public String login;
+    public String password;
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
