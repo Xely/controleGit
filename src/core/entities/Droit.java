49d826935df1494c25428c09d85889abec8447be
geoffrey <geoffrey.fabbro@itga.fr>
2017-01-10 16:53:04 +0100

    get set droit

--- src/core/entities/Droit.java
+++ src/core/entities/Droit.java
@@ -1,3 +1,47 @@
 public class Droit {
+    public String niveau;
+    public String sévérité;
     public String nom;
+    public String société;
+
+    
+    public String niveau() 
+    {
+        return niveau;
+    }
+
+    public String sévérité() 
+    {
+        return sévérité;
+    }
+    
+    public String nom() 
+    {
+        return nom;
+    }
+    
+   public String société() 
+   {
+        return société;
+   }
+
+   public void setPrenom(String niveau) 
+   {
+        this.niveau = niveau;
+   }
+
+    public void setPrenom(String sévérité) 
+    {
+        this.sévérité = sévérité;
+    }
+
+    public void setPrenom(String nom) 
+    {
+        this.nom = nom;
+    }
+
+    public void setPrenom(String société) 
+    {
+        this.société = société;
+    }
 }
