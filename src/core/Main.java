1a54f64309d7d2fb3e0f68508c8d0394d1222e4d
geoffrey <geoffrey.fabbro@itga.fr>
2017-02-07 15:06:52 +0100

    Add user and droit

---
+++ src/core/Main.java
@@ -0,0 +1,37 @@
+
+
+import java.util.ArrayList;
+
+import entities.Droit;
+import entities.User;
+
+public class Main {
+
+    public static void main(String[] args) {
+    	
+    	User user = new User("Geoffrey","Fabbro","Ziguard","Ziguard");
+    	User user1 = new User("Julien","","Xelty","Xelty");
+    	
+    	
+    	Droit droit = new Droit ();
+    	Droit droit1 = new Droit ();
+    	
+    	
+    	Droit droit3 = new Droit ();
+    	Droit droit4 = new Droit ();
+    	
+    	
+    	ArrayList<Droit> listeDroitUser1 = new ArrayList<Droit>();
+    	ArrayList<Droit> listeDroitUser = new ArrayList<Droit>();
+    	
+    	listeDroitUser.add(droit);
+    	listeDroitUser.add(droit1);
+    	listeDroitUser1.add(droit4);
+    	listeDroitUser1.add(droit3);
+    	
+    	user.setDroits(listeDroitUser);
+    	user1.setDroits(listeDroitUser1);
+    	
+    }
+}
+
