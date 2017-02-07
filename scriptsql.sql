ba0a53fe63826463c4a2e53c674efdbbe9cb2d48
geoffrey <geoffrey.fabbro@itga.fr>
2017-02-07 16:23:11 +0100

    Suppression insertion droit et users

--- scriptsql.sql
+++ scriptsql.sql
@@ -46,11 +46,11 @@ ALTER TABLE AttributionDroit ADD CONSTRAINT FK_AttributionDroit_id FOREIGN KEY (
 ALTER TABLE AttributionDroit ADD CONSTRAINT FK_AttributionDroit_id_User FOREIGN KEY (id_User) REFERENCES User(id);
 
 
-INSERT INTO user (id,nom,prenom,login,password,droits)VALUES(1,"fabbro","geoffrey","Ziguard","Ziguard",1)
-INSERT INTO user (id,nom,prenom,login,password,droits)VALUES(2,"guyader","Julien","Xelty","Xelty",2)
+-- INSERT INTO user (id,nom,prenom,login,password,droits)VALUES(1,"fabbro","geoffrey","Ziguard","Ziguard",1)
+-- INSERT INTO user (id,nom,prenom,login,password,droits)VALUES(2,"guyader","Julien","Xelty","Xelty",2)
 
-INSERT INTO droit(id,niveau,nom,severite,societe) VALUES (1,1,"TEST1","TEST1","TEST1")
-INSERT INTO droit(id,niveau,nom,severite,societe) VALUES (2,2,"TEST2","TEST2","TEST2")
-INSERT INTO droit(id,niveau,nom,severite,societe) VALUES (3,3,"TEST3","TEST3","TEST3")
-INSERT INTO droit(id,niveau,nom,severite,societe) VALUES (4,4,"TEST4","TEST4","TEST4")
+-- INSERT INTO droit(id,niveau,nom,severite,societe) VALUES (1,1,"TEST1","TEST1","TEST1")
+-- INSERT INTO droit(id,niveau,nom,severite,societe) VALUES (2,2,"TEST2","TEST2","TEST2")
+-- INSERT INTO droit(id,niveau,nom,severite,societe) VALUES (3,3,"TEST3","TEST3","TEST3")
+-- INSERT INTO droit(id,niveau,nom,severite,societe) VALUES (4,4,"TEST4","TEST4","TEST4")
 
